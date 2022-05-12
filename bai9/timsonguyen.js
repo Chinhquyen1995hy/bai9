c5fab28885f2ba1f80a6a085795fef49542a2547
Chinhquyen1995hy <minhchinh1995hy@gmail.com>
2022-05-12 17:02:46 +0700

    20sofibonacci

---
+++ bai9/timsonguyen.js
@@ -0,0 +1,21 @@
+let number=prompt('nhap so')
+
+let i=2
+let songuyento=1;
+    else if (number==2){
+        document.write('la so nguyen to')
+    }
+    else if (number>2){
+        while (i<n){
+            if (n%i==0){
+                songuyento=0
+                break
+            };
+            i++
+        }
+        if (songuyento===1){
+            document.write('la so nguyen to')
+        }
+    }
+
+}
